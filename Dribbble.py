10161479af18897423f991438f5b38c259a462e6
Sergey Azarkin <hobagos@gmail.com>
2012-12-28 23:12:20 +0400

    Add Dribble API

---
+++ Dribbble.py
@@ -0,0 +1,15 @@
+import urllib2
+import simplejson
+
+
+class Dribbble:
+
+	def __init__(self):
+		self.opener = urllib2.build_opener()
+
+	def get_popular(self):
+		req = urllib2.Request("http://api.dribbble.com/shots/popular")
+		answer = simplejson.load(
+			self.opener.open(req)
+			)
+		return answer['shots'][:1]
