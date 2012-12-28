ea41e6bfbfe56bbc1a2e1a931d3c19143b4cc805
Sergey Azarkin <hobagos@gmail.com>
2012-12-29 00:38:05 +0400

    All images loads async

--- Thread.py
+++ Thread.py
@@ -1,25 +1,45 @@
 import threading
 import urllib2
+from random import shuffle
 from gi.repository import Gdk, GdkPixbuf
 
 import Dribbble
 
 
+
 class ImageLoader(threading.Thread):
-    def __init__(self, image_widget):
+    def __init__(self, image_widgets):
         threading.Thread.__init__(self)
 
-        self.image_widget = image_widget
+        self.image_widgets = image_widgets
 
     def run(self):
-        dribbble = Dribbble.Dribbble()
-        dribbble_shots = dribbble.get_popular()
+        try:
+            dribbble = Dribbble.Dribbble()
+            dribbble_shots = dribbble.get_popular()[:len(self.image_widgets)]
+            shuffle(dribbble_shots)
+        except Exception, e:
+            print e
+            return
 
-        for shot in dribbble_shots[:1]:
-            response = urllib2.urlopen(shot['image_teaser_url'])
-            loader = GdkPixbuf.PixbufLoader()
-            loader.write(response.read())
-            loader.close()
-            Gdk.threads_enter()
-            self.image_widget.set_from_pixbuf(loader.get_pixbuf())
-            Gdk.threads_leave()
+        for i, shot in enumerate(dribbble_shots):
+            ImageFromURL(self.image_widgets[i], shot['image_teaser_url'])
+
+class ImageFromURL(threading.Thread):
+    def __init__(self, image_widget, url):
+        threading.Thread.__init__(self)
+
+        self.url = url
+        self.image_widget = image_widget
+        self.start()
+
+    def run(self):
+        response = urllib2.urlopen(self.url)
+
+        loader = GdkPixbuf.PixbufLoader()
+        loader.write(response.read())
+        loader.close()
+
+        Gdk.threads_enter()
+        self.image_widget.set_from_pixbuf(loader.get_pixbuf())
+        Gdk.threads_leave()
